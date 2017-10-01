c0e62b1aa032d6ab245425bed219802fa71f6286
mgechev <mgechev@gmail.com>
2017-09-30 18:54:34 -0700

    refactor: drop the ts directories

--- app/ch6/injector-basics/basics/app.ts
+++ app/ch6/injector-basics/basics/app.ts
@@ -11,14 +11,14 @@ import { BrowserModule } from '@angular/platform-browser';
 const BUFFER_SIZE = new InjectionToken<number>('buffer-size');
 
 class Buffer {
-  constructor(@Inject(BUFFER_SIZE) private size: Number) {
+  constructor( @Inject(BUFFER_SIZE) private size: Number) {
     console.log(this.size);
   }
 }
 
 @Injectable()
 class Socket {
-  constructor(private buffer: Buffer) {}
+  constructor(private buffer: Buffer) { }
 }
 
 @Component({
@@ -37,6 +37,6 @@ class AppComponent {
   bootstrap: [AppComponent],
   imports: [BrowserModule]
 })
-class AppModule {}
+class AppModule { }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- tools/utils/template_locals.ts
+++ tools/utils/template_locals.ts
@@ -1,15 +1,18 @@
-import {APP_BASE, APP_DEST, APP_ROOT, APP_SRC, APP_TITLE, SYSTEM_CONFIG, VERSION} from '../config';
+import { APP_BASE, APP_DEST, APP_ROOT, APP_SRC, APP_TITLE, SYSTEM_CONFIG, VERSION } from '../config';
 import * as fs from 'fs';
-import {join, dirname, sep} from 'path';
+import { join, sep } from 'path';
 import * as jsonfile from 'jsonfile';
 
 const CHAPTERS_MAP = {
+  ch1: 'Chapter 1',
+  ch2: 'Chapter 2',
   ch3: 'Chapter 3',
   ch4: 'Chapter 4',
   ch5: 'Chapter 5',
   ch6: 'Chapter 6',
   ch7: 'Chapter 7',
-  ch8: 'Chapter 8'
+  ch8: 'Chapter 8',
+  ch9: 'Chapter 9'
 };
 
 function listMetadataStrategy(data) {
@@ -24,8 +27,8 @@ function readMetadata(current, appRoot) {
   fs.readdirSync(current).forEach(function (file) {
     if (file === 'meta.json') {
       file = join('.', sep, current, file);
-      let currentChapterAbr = file.match(/(ch\d+)/)[0];
-      let chapter = CHAPTERS_MAP[currentChapterAbr];
+      const currentChapterAbr = file.match(/(ch\d+)/)[0];
+      const chapter = CHAPTERS_MAP[currentChapterAbr];
       result.push({
         chapter,
         file: current,
@@ -39,10 +42,9 @@ function readMetadata(current, appRoot) {
 }
 
 function getMetadata(appRoot) {
-  let metadata = readMetadata(appRoot, appRoot);
-  metadata = metadata.sort((a, b) => {
-    let chapterA = parseInt(a.chapter.match(/\d+/)[0]);
-    let chapterB = parseInt(b.chapter.match(/\d+/)[0]);
+  const metadata = readMetadata(appRoot, appRoot).sort((a, b) => {
+    const chapterA = parseInt(a.chapter.match(/\d+/)[0]);
+    const chapterB = parseInt(b.chapter.match(/\d+/)[0]);
     if (chapterA < chapterB) {
       return -1;
     } else if (chapterA > chapterB) {
@@ -54,23 +56,8 @@ function getMetadata(appRoot) {
       return 1;
     }
   });
-  let items = metadata.map(listMetadataStrategy);
+  const items = metadata.map(listMetadataStrategy);
   return '<ol>' + items.join('\n') + '</ol>';
-}
-
-function transformPath(plugins, env) {
-  return function (filepath) {
-    var filename = filepath.replace(sep + APP_DEST, '');
-    arguments[0] = join(APP_BASE, filename);
-    return plugins.inject.transform.apply(plugins.inject.transform, arguments);
-  };
-}
-
-function relativePath(fileLocation) {
-  fileLocation = dirname(fileLocation);
-  var parentDir = __dirname.replace(join('tools'), '');
-  var result = join(sep, fileLocation.replace(join(parentDir, 'app'), ''), sep);
-  return result;
 }
 
 // TODO: Add an interface to register more template locals.
